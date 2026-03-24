99b5573e9529b0e2ef0f7c36dcf6520802acae76
Omar Ismail <omarkalifa027@gmail.com>
2026-03-25 00:01:16 +0200

    exercise 1.8 done

--- part 1/exercise 1.6/src/App.jsx
+++ part 1/exercise 1.6/src/App.jsx
@@ -1,7 +1,26 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+
+  const { good, neutral, bad } = props
+  return (
+  <div>
+
+      <h1>statistics.</h1>
+
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {good + neutral + bad === 0 ? 0 : (good - bad) / (good + neutral + bad)}</p>
+      <p>positive {good + neutral + bad === 0 ? 0 : (good / (good + neutral + bad) * 100)} %</p>
+
+  </div>
+  )
+}
+
 const App = () => {
-  // save clicks of each button to its own state
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -16,14 +35,7 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      <h1>statistics.</h1>
-
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {good + neutral + bad === 0 ? 0 : (good - bad) / (good + neutral + bad)}</p>
-      <p>positive {good + neutral + bad === 0 ? 0 : (good / (good + neutral + bad) * 100)} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
 
     </div>
     
